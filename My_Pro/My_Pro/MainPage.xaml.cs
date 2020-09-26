b015010ca8005d34b46f392a99bd19fc6a040554
Anjali Ajith <anjusukumari@gmail.com>
2020-09-26 16:16:48 +0530

    Importing Project

---
+++ My_Pro/My_Pro/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace My_Pro
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            bool resp = await DisplayAlert("Save ? ","Do you want to continue ? ", "yes", "No");
+            Console.WriteLine("Save ?", resp);
+        }
+    }
+}
